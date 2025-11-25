eb71b0929109043e7afa2ad79c1d721bf073c290
Zachari Vaia <zvayaa@gmail.com>
2025-11-25 16:11:19 +0000

    3dgs

--- train.sh
+++ train.sh
@@ -8,7 +8,7 @@ sudo chmod -R u+rwX /home/vaia/ResGS/output
 
 # ====== PATHS ======
 DATASET_PATH="${DATASET_PATH:-/home/vaia/ResGS/data}"
-SAVE_PATH="${SAVE_PATH:-/home/vaia/ResGS/output/blurgs_opencv_25_13_2500_6000_abs_tb_full_eval}"
+SAVE_PATH="${SAVE_PATH:-/home/vaia/ResGS/output/blur_gs_opencv_25_13_2500_6000_3dgs_tb_full_eval}"
 
 # ====== RUN DOCKER ======
 docker run -it --rm  \
@@ -23,7 +23,7 @@ docker run -it --rm  \
       python -u /app/script.py \
         --eval \
         --dataset_path /app/data \
-        --save_path /app/output/blurgs_opencv_25_13_2500_6000_abs_tb_full_eval ;
+        --save_path /app/output/blur_gs_opencv_25_13_2500_6000_3dgs_tb_full_eval ;
 
       # ===== FIX PERMISSIONS INSIDE CONTAINER =====
       chown -R 1000:1000 /app/output
